ffafd729d199bc7e11be4e0f77d7cac4a0753439
papermashea <molloy.shea@gmail.com>
2017-11-19 17:39:59 -0500

    test files for php webform

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+if(isset($_POST['submit'])){
+
+    //collect form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    //check name is set
+    if($name ==''){
+        $error[] = 'Name is required';
+    }
+
+    //check for a valid email address
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+         $error[] = 'Please enter a valid email address';
+    }
+
+    //if no errors carry on
+    if(!isset($error)){
+
+        # Title of the CSV
+        $Content = "Name, Email\n";
+
+        //set the data of the CSV
+        $Content .= "$name, $email\n";
+
+        # set the file name and create CSV file
+        $FileName = "formdata-".date("d-m-y-h:i:s").".csv";
+        header('Content-Type: application/csv'); 
+        header('Content-Disposition: attachment; filename="' . $FileName . '"'); 
+        echo $Content;
+        exit();
+    }
+}
+
+//if their are errors display them
+if(isset($error)){
+    foreach($error as $error){
+        echo "<p style='color:#ff0000'>$error</p>";
+    }
+}
+?> 
+
+<html>
+<form action='' method='post'>
+<p><label>Name</label><br><input type='text' name='name' value=''></p> 
+<p><label>Email</label><br><input type='text' name='email' value=''></p> 
+<p><input type='submit' name='submit' value='Submit'></p> 
+</form>
+</html>
+<form action='' method='post'>
+<p><label>Name</label><br><input type='text' name='name' value=''></p> 
+<p><label>Email</label><br><input type='text' name='email' value=''></p> 
+<p><input type='submit' name='submit' value='Submit'></p> 
+</form>
